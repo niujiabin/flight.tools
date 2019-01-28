fec54ab0215726fb039db63ad7f90ce8d0ed3f92
jiabin.niu <njb19920913@163.com>
2019-01-29 00:58:24 +0800

    aaa

---
+++ src/main/java/com/qunar/tools/flight/tools/util/QArrayBlockingQueue.java
@@ -0,0 +1,136 @@
+package com.qunar.tools.flight.tools.util;
+
+import java.util.AbstractQueue;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author : jiabin.niu
+ **/
+public class QArrayBlockingQueue<E> extends AbstractQueue<E> implements BlockingQueue<E> {
+
+    /**
+     * 存储数组的元素
+     */
+    private Object[] items;
+
+    private ReentrantLock lock;
+    /**
+     * 当前队列大小
+     */
+    private int count;
+    private int putIndex;
+
+    public QArrayBlockingQueue(int capacity) {
+        items = new Object[capacity];
+        lock = new ReentrantLock();
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(E e) throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public E take() throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public E poll(long timeout, TimeUnit unit) throws InterruptedException {
+        return null;
+    }
+
+    @Override
+    public int remainingCapacity() {
+        return 0;
+    }
+
+    @Override
+    public int drainTo(Collection<? super E> c) {
+        return 0;
+    }
+
+    @Override
+    public int drainTo(Collection<? super E> c, int maxElements) {
+        return 0;
+    }
+
+    /**
+     * 如果能添加到队列 返回true，不能添加到队列直接返回false
+     *
+     * @param e
+     * @return
+     */
+    @Override
+    public boolean offer(E e) {
+        //使用一个不可变的引用 指向lock，保证本次offer操作的lock不会发生改变
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            if (count == items.length) {
+                return false;
+            }
+            //队列没有满直接入队  肯定返回成功
+            enqueue(e);
+            return true;
+        } catch (Exception exception) {
+
+        } finally {
+            lock.unlock();
+        }
+
+        return false;
+    }
+
+    private void enqueue(E e) {
+        final Object[] items = this.items;
+        items[putIndex] = e;
+        if (++putIndex == items.length){
+            putIndex = 0;
+        }
+        count++;
+        //唤醒非空等待队列
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+
+    /**
+     * 如果添加失败 直接抛出异常
+     *
+     * @param e
+     * @return
+     */
+    @Override
+    public boolean add(E e) {
+        return super.add(e);
+    }
+}
