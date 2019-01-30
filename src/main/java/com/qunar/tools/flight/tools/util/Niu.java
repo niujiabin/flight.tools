263262a6cbc0296470d487e85712c48ee166573a
jiabin.niu <jiabin.niu@qunar.com>
2019-01-30 16:12:17 +0800

    n

---
+++ src/main/java/com/qunar/tools/flight/tools/util/Niu.java
@@ -0,0 +1,11 @@
+package com.qunar.tools.flight.tools.util;
+
+/**
+ * Created by jiabin.niu on 2019.01.29 12:02
+ */
+public class Niu implements  Test {
+
+    public Niu() {
+        System.out.println("123");
+    }
+}
---
+++ src/main/java/com/qunar/tools/flight/tools/util/Test.java
@@ -0,0 +1,7 @@
+package com.qunar.tools.flight.tools.util;
+
+/**
+ * Created by jiabin.niu on 2019.01.29 11:59
+ */
+public interface Test {
+}
