5b8fb95f8a7530cb2fd4fc2007d77f6f049c6c46
jiabin.niu <jiabin.niu@qunar.com>
2019-02-01 19:39:13 +0800

    leetcode problem

---
+++ src/main/java/com/qunar/tools/flight/tools/leetcode/leetcode647回文子串.java
@@ -0,0 +1,14 @@
+package com.qunar.tools.flight.tools.leetcode;
+
+/**
+ * Created by jiabin.niu on 2019.02.01 17:34
+ */
+public class leetcode647回文子串 {
+
+    public int countSubstrings(String s) {
+
+        //代码和最长回文子串类似
+        return 0;
+    }
+
+}
