6838037c104e5a3feae230f380b27ed8957b4b00
jiabin.niu <jiabin.niu@qunar.com>
2019-02-12 21:44:32 +0800

    leetcode

--- src/main/java/com/qunar/tools/flight/tools/leetcode/LeetCode最接近的三数之和.java
+++ src/main/java/com/qunar/tools/flight/tools/leetcode/LeetCode最接近的三数之和.java
@@ -1,7 +1,6 @@
 package com.qunar.tools.flight.tools.leetcode;
 
 /**
- *
  * @author jiabin.niu
  * @date 2019.02
  */
@@ -9,6 +8,8 @@ public class LeetCode最接近的三数之和 {
 
     public int threeSumClosest(int[] nums, int target) {
 
+
+        return 0;
     }
 
     public static void main(String[] args) {
